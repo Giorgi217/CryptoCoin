f59a0b055397274f1741f5328e81806efd2735ae
Giorgi Amiranashvili <giorgi@Giorgis-MacBook-Pro.local>
2025-01-13 18:02:38 +0400

    initial commit with gitignore

---
+++ CryptoCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoCoin
+//
+//  Created by Giorgi Amiranashvili on 13.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
